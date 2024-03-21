9cd8169ae9927d57bdbdb6eab18c30fc9e8ff14f
Aman Chaurasiya <amanchaurasiya786@gmail.com>
2024-03-21 16:27:06 +0530

    mongo db connected and utils file done

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import {app} from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:"./.env"
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB connection failed !!!", error);
+  });
+
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,47 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudnary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been successfully uplaoaded
+    console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved file as the upload operation got failed
+    return null;
+  }
+};
+
+const deleteOnCloudniary = async (fileUrl) => {
+  try {
+    if (!fileUrl) {
+      return "!file url";
+    }
+    const imageArray = fileUrl.split("/");
+
+    const imageName = imageArray[imageArray.length - 1].split(".")[0];
+
+    const result = await cloudinary.uploader.destroy(
+      imageName,
+      (error, result) => {
+        console.log(error);
+      }
+    );
+    return result;
+  } catch (error) {
+    return "error occured";
+  }
+};
+export { uploadOnCloudinary, deleteOnCloudniary };
