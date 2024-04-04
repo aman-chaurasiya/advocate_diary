2e6a79a996bb27490434a390c1bd9cc35fde2e48
Aman Chaurasiya <amanchaurasiya786@gmail.com>
2024-04-04 12:51:03 +0530

    login logout api done and multer bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,29 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "./../utils/ApiResponse.js";
+import fs from "fs";
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = user.generateAccessToken();
+
+    // console.log(accessToken, "==========>");
+    const refreshToken = user.generateRefreshToken();
+    // console.log(refreshToken, "==========>");
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "somthing went wrong while generating refresh and access token"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, resp) => {
   const { firstName, lastName, email, mobile, password } = req.body;
@@ -14,19 +37,20 @@ const registerUser = asyncHandler(async (req, resp) => {
   ) {
     throw new ApiError(400, "All fields are required!!");
   }
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing!!");
+  }
 
   const existedUser = await User.findOne({
     $or: [{ email }, { mobile }],
   });
 
   if (existedUser) {
+    fs.unlinkSync(avatarLocalPath);
     throw new ApiError(409, "User with email or mobile already exist!!");
   }
 
-  const avatarLocalPath = req.file?.path;
-  if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is missing!!");
-  }
   const avatar = await uploadOnCloudinary(avatarLocalPath, "Avatars");
   if (!avatar) {
     throw new ApiError(400, "Avatar file is required");
@@ -52,9 +76,77 @@ const registerUser = asyncHandler(async (req, resp) => {
     .json(new ApiResponse(200, createdUser, "User register successfully!!"));
 });
 
+const loginUser = asyncHandler(async (req, resp) => {
+  const { email, password, mobile } = req.body;
+
+  if (!mobile && !email) {
+    throw new ApiError(400, "username or password is required");
+  }
+  const user = await User.findOne({
+    $or: [{ mobile }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "invalid user cridentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return resp
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User login successfully!!"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, resp) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return resp
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
 // const testing = asyncHandler(async (req, resp) => {
 //   const avatar = req.file.path;
 //   return resp.status(200).json(new ApiResponse(200, avatar, " successfully:"));
 // });
 
-export { registerUser };
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewere.js";
+
+import { verifyJWT } from "./../middlewares/auth.middlewere.js";
 const router = Router();
 
 router.route("/register").post(upload.single("avatar"), registerUser);
+router.route("/login").post(loginUser);
+
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 // router.route("/testing").post(upload.single("avatar"), testing);
 
